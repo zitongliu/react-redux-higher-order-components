bb0f08ea884a0607421717df0b5e8d66b6ff4ed4
zitongliu <zitongliu7@gmail.com>
2017-08-24 14:49:40 +1000

    updated readme

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import Header from './header';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Header />
+      </div>
     );
   }
 }
-
-function mapStateToProps(state) {
-  return { posts: state.posts };
-}
-
-export default connect(mapStateToProps)(App);
