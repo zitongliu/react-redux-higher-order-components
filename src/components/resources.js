9c2b137ce50c7eefe759700fd318ea4dec97c39e
zitongliu <zitongliu7@gmail.com>
2017-08-24 15:06:18 +1000

    set up react router 2

---
+++ src/components/resources.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default () => {
+  return (
+    <div>
+      Super Special Secret Recipe
+      <ul>
+        <li>1 Cup Sugar</li>
+        <li>2 Cups Salt</li>
+        <li>1 Cup Flour</li>
+      </ul>
+    </div>
+  )
+}
